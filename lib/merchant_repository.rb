5f5706b06155918da46de99c5a0fabdb58757864
Andy Mention <amention@gmail.com>
2014-06-30 15:38:55 -0600

    Add merchant repository find by/find all by

--- lib/merchant_repository.rb
+++ lib/merchant_repository.rb
@@ -21,15 +21,22 @@ class MerchantRepository
   def random
     merchants.sample
   end
-  #
-  # def find_by_X(match)
-  #   # where X is some attribute, returns a single instance whose X attribute case-insensitive attribute matches the match parameter. For instance, Customer.find_by_first_name("Mary") could find a Customer with the first name attribute "Mary" or "mary" but not "Mary Ellen".
-  # end
-  #
-  # def find_all_by_X(match)
-  #   #  works just like find_by_X except it returns a collection of all matches. If there is no match, it returns an empty Array.
-  # end
-  #
+  
+  def find_by_id(match)
+    merchants.detect { |merchant| merchant.id.downcase == match.downcase }
+  end
+  
+  def find_by_name(match)
+    merchants.detect { |merchant| merchant.name.downcase == match.downcase }
+  end
+
+  def find_all_by_id(match)
+    merchants.select { |merchant| merchant.id.downcase == match.downcase }
+  end
+  
+  def find_all_by_name(match)
+    merchants.select { |merchant| merchant.name.downcase == match.downcase }
+  end
   # def most_revenue(X)
   #   # returns the top x merchant instances ranked by total revenue
   # end
