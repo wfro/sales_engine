c690ddafcea906cf71ba271de315abc3c905b23f
Andy Mention <amention@gmail.com>
2014-06-30 16:57:01 -0600

    Adds invoice item repository find/find all methods

--- lib/invoice_item_repository.rb
+++ lib/invoice_item_repository.rb
@@ -22,11 +22,27 @@ class InvoiceItemRepository
     invoice_items.sample
   end
   
-  # def find_by_X(match)
-  #   # where X is some attribute, returns a single instance whose X attribute case-insensitive attribute matches the match parameter. For instance, Customer.find_by_first_name("Mary") could find a Customer with the first name attribute "Mary" or "mary" but not "Mary Ellen".
-  # end
-  #
-  # def find_all_by_X(match)
-  #   #  works just like find_by_X except it returns a collection of all matches. If there is no match, it returns an empty Array.
-  # end
+  def find_by_item_id(match)
+    invoice_items.detect {|invoice_item| invoice_item.item_id == match }
+  end
+  
+  def find_by_quantity(match)
+    invoice_items.detect {|invoice_item| invoice_item.quantity == match }
+  end
+  
+  def find_by_id(match)
+    invoice_items.detect {|invoice_item| invoice_item.id == match }
+  end
+  
+  def find_all_by_item_id(match)
+    invoice_items.select {|invoice_item| invoice_item.item_id == match }
+  end
+  
+  def find_all_by_quantity(match)
+    invoice_items.select {|invoice_item| invoice_item.quantity == match }
+  end
+  
+  def find_all_by_id(match)
+    invoice_items.select {|invoice_item| invoice_item.id == match }
+  end
 end
--- test/invoice_item_repository_test.rb
+++ test/invoice_item_repository_test.rb
@@ -14,4 +14,52 @@ class InvoiceItemRepositoryTest < Minitest::Test
   def test_it_returns_all_invoice_items
     assert invoice_item_repository.all.length >= 10
   end
+  
+  def test_it_finds_by_item_id
+    results = invoice_item_repository.find_by_item_id('539')
+    assert_equal results.id, "1"
+
+    results = invoice_item_repository.find_by_item_id('539')
+    refute_equal results.id, "2"
+  end
+  
+  def test_it_finds_by_quantity
+    results = invoice_item_repository.find_by_quantity('5')
+    assert_equal results.id, "1"
+
+    results = invoice_item_repository.find_by_quantity('5')
+    refute_equal results.id, "2"
+  end
+  
+  def test_it_finds_by_id
+    results = invoice_item_repository.find_by_id('1')
+    assert_equal results.quantity, "5"
+
+    results = invoice_item_repository.find_by_id('1')
+    refute_equal results.quantity, "9"
+  end
+  
+  def test_it_finds_all_by_item_id
+    results = invoice_item_repository.find_all_by_item_id('539')
+    assert_equal results[0].id, "1"
+
+    results = invoice_item_repository.find_all_by_item_id('539')
+    refute_equal results[0].id, "2"
+  end
+  
+  def test_it_finds_all_by_quantity
+    results = invoice_item_repository.find_all_by_quantity('5')
+    assert_equal results[0].id, "1"
+
+    results = invoice_item_repository.find_all_by_quantity('5')
+    refute_equal results[0].id, "2"
+  end
+  
+  def test_it_finds_all_by_id
+    results = invoice_item_repository.find_all_by_id('1')
+    assert_equal results[0].quantity, "5"
+
+    results = invoice_item_repository.find_all_by_id('1')
+    refute_equal results[0].quantity, "9"
+  end
 end
