4a69d91c20167e73ba067697b84ccee9085b21db
Will Faurot <wfaurot@gmail.com>
2014-06-30 14:01:16 -0600

    Add Invoice/InvoiceRepository

--- lib/invoice_repository.rb
+++ lib/invoice_repository.rb
@@ -1,16 +1,29 @@
 class InvoiceRepository
-  def all
+  def self.load(filename="./data/invoices.csv")
+    rows = CSV.open(filename, headers: true, header_converters: :symbol)
+    invoices = rows.map { |row| Invoice.new(row) }
+    new(invoices)
   end
-  
-  def random
-    # returns a random instance
+
+
+  attr_reader :invoices
+
+  def initialize(invoices)
+    @invoices = invoices
   end
-  
-  def find_by_X(match)
-    # where X is some attribute, returns a single instance whose X attribute case-insensitive attribute matches the match parameter. For instance, Customer.find_by_first_name("Mary") could find a Customer with the first name attribute "Mary" or "mary" but not "Mary Ellen".
-  end
-  
-  def find_all_by_X(match)
-    #  works just like find_by_X except it returns a collection of all matches. If there is no match, it returns an empty Array.
-  end
+
+  # def all
+  # end
+  #
+  # def random
+  #   # returns a random instance
+  # end
+  #
+  # def find_by_X(match)
+  #   # where X is some attribute, returns a single instance whose X attribute case-insensitive attribute matches the match parameter. For instance, Customer.find_by_first_name("Mary") could find a Customer with the first name attribute "Mary" or "mary" but not "Mary Ellen".
+  # end
+  #
+  # def find_all_by_X(match)
+  #   #  works just like find_by_X except it returns a collection of all matches. If there is no match, it returns an empty Array.
+  # end
 end
