66c70460063965750531fe6000162f041503d931
Will Faurot <wfaurot@gmail.com>
2014-06-30 12:58:53 -0600

    Add Customer/CustomerRepository

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,13 +1,27 @@
 class Customer
-  def invoices
-    # returns a collection of Invoice instances associated with this object.
+  attr_reader :id,
+              :first_name,
+              :last_name,
+              :created_at,
+              :updated_at
+
+  def initialize(data)
+    @id         = data[:id]
+    @first_name = data[:first_name]
+    @last_name  = data[:last_name]
+    @created_at = data[:created_at]
+    @updated_at = data[:updated_at]
   end
-  
-  def transactions
-    # returns an array of Transaction instances associated with the customer
-  end
-  
-  def favorite_merchant
-    # returns an instance of Merchant where the customer has conducted the most successful transactions
-  end
+
+  # def invoices
+  #   # returns a collection of Invoice instances associated with this object.
+  # end
+  #
+  # def transactions
+  #   # returns an array of Transaction instances associated with the customer
+  # end
+  #
+  # def favorite_merchant
+  #   # returns an instance of Merchant where the customer has conducted the most successful transactions
+  # end
 end
