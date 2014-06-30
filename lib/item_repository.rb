733be417e85ea03290f33984900c2722a89e3926
Andy Mention <amention@gmail.com>
2014-06-30 16:40:49 -0600

    Adds find/fnd all by functions to item repository

--- lib/item_repository.rb
+++ lib/item_repository.rb
@@ -15,18 +15,33 @@ class ItemRepository
     @items
   end
 
-  # def random
-  #   # returns a random instance
-  # end
-  #
-  # def find_by_X(match)
-  #   # where X is some attribute, returns a single instance whose X attribute case-insensitive attribute matches the match parameter. For instance, Customer.find_by_first_name("Mary") could find a Customer with the first name attribute "Mary" or "mary" but not "Mary Ellen".
-  # end
-  #
-  # def find_all_by_X(match)
-  #   #  works just like find_by_X except it returns a collection of all matches. If there is no match, it returns an empty Array.
-  # end
-  #
+  def random
+    items.sample
+  end
+  
+  def find_by_name(match)
+    items.detect {|item| item.name.downcase == match.downcase}
+  end
+  
+  def find_by_unit_price(match)
+    items.detect {|item| item.unit_price.downcase == match.downcase}
+  end
+  
+  def find_by_merchant_id(match)
+    items.detect {|item| item.merchant_id.downcase == match.downcase}
+  end
+  
+  def find_all_by_name(match)
+    items.select {|item| item.name.downcase == match.downcase}
+  end
+  
+  def find_all_by_unit_price(match)
+    items.select {|item| item.unit_price.downcase == match.downcase}
+  end
+  
+  def find_all_by_merchant_id(match)
+    items.select {|item| item.merchant_id.downcase == match.downcase}
+  end
   # def most_revenue(x)
   #   # returns the top x item instances ranked by total revenue generated
   # end
--- test/item_repository_test.rb
+++ test/item_repository_test.rb
@@ -12,4 +12,52 @@ class ItemRepositoryTest < Minitest::Test
   def test_it_loads_Item_objects
     assert_equal "Item Qui Esse", item_repository.items[0].name
   end
+
+  def test_it_finds_by_name
+    results = item_repository.find_by_name('Item Qui Esse')
+    assert_equal results.id, "1"
+
+    results = item_repository.find_by_name('Item Qui Esse')
+    refute_equal results.id, "2"
+  end
+  
+  def test_it_finds_by_unit_price
+    results = item_repository.find_by_unit_price('75107')
+    assert_equal results.id, "1"
+
+    results = item_repository.find_by_unit_price('75107')
+    refute_equal results.id, "2"
+  end
+  
+  def test_it_finds_by_merchant_id
+    results = item_repository.find_by_merchant_id('1')
+    assert_equal results.id, "1"
+
+    results = item_repository.find_by_merchant_id('1')
+    refute_equal results.id, "2"
+  end
+  
+  def test_it_finds_all_by_name
+    results = item_repository.find_all_by_name('Item Qui Esse')
+    assert_equal results[0].id, "1"
+
+    results = item_repository.find_all_by_name('Item Qui Esse')
+    refute_equal results[0].id, "2"
+  end
+  
+  def test_it_finds_all_by_unit_price
+    results = item_repository.find_all_by_unit_price('75107')
+    assert_equal results[0].id, "1"
+
+    results = item_repository.find_all_by_unit_price('75107')
+    refute_equal results[0].id, "2"
+  end
+  
+  def test_it_finds_all_by_merchant_id
+    results = item_repository.find_all_by_merchant_id('1')
+    assert_equal results[0].id, "1"
+
+    results = item_repository.find_all_by_merchant_id('1')
+    refute_equal results[0].id, "2"
+  end
 end
