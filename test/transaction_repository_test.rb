e9803657e67c540ee8acaf8a7669b010176b9307
Will Faurot <will@localhost.localdomain>
2014-06-30 20:13:49 -0600

    Add more finding

--- test/transaction_repository_test.rb
+++ test/transaction_repository_test.rb
@@ -15,4 +15,44 @@ class TransactionRepositoryTest < Minitest::Test
   def test_returns_all_transactions
     assert transaction_repository.all.length >= 10
   end
+
+  def test_finds_by_id
+    result = transaction_repository.find_by_id('1')
+    assert_equal '4654405418249632', result.credit_card_number
+  end
+  
+  def test_finds_by_invoice_id
+    result = transaction_repository.find_by_invoice_id('1')
+    assert_equal '4654405418249632', result.credit_card_number
+  end
+
+  def test_finds_by_credit_card_number
+    result = transaction_repository.find_by_credit_card_number('4654405418249632')
+    assert_equal '1', result.id
+  end
+
+  def test_finds_by_result
+    result = transaction_repository.find_by_result('success')
+    assert_equal '1', result.id
+  end
+
+  def test_finds_all_by_id
+    result = transaction_repository.find_all_by_id('1')
+    assert_equal '4654405418249632', result[0].credit_card_number
+  end
+  
+  def test_finds_all_by_invoice_id
+    result = transaction_repository.find_all_by_invoice_id('1')
+    assert_equal '4654405418249632', result[0].credit_card_number
+  end
+
+  def test_finds_all_by_credit_card_number
+    result = transaction_repository.find_all_by_credit_card_number('4654405418249632')
+    assert_equal '1', result[0].id
+  end
+
+  def test_finds_all_by_result
+    result = transaction_repository.find_all_by_result('success')
+    assert_equal '1', result[0].id
+  end
 end
