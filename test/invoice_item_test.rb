9227b9b5052201abf2adf54c46cc29fd1eb84800
Will Faurot <wfaurot@gmail.com>
2014-07-02 18:09:00 -0600

    Clean up

--- test/invoice_item_test.rb
+++ test/invoice_item_test.rb
@@ -1,27 +1,14 @@
 require './test/test_helper'
 
 class InvoiceItemTest < Minitest::Test
-  
-  def entry
-    {
-    id: '1',
-    item_id: '539',
-    invoice_id: '1',
-    quantity: '5',
-    unit_price: '13635',
-    created_at: 'time',
-    updated_at: 'time2'
-    }
+  attr_reader :invoice_item
+
+  def setup
+    engine = SalesEngine.new('./test/fixtures')
+    @invoice_item = engine.invoice_item_repository.invoice_items[0]
   end
-  
+
   def test_attributes
-    invoice_item = InvoiceItem.new(entry)
     assert_equal 1, invoice_item.id
-    assert_equal 539, invoice_item.item_id
-    assert_equal 1, invoice_item.invoice_id
-    assert_equal 5, invoice_item.quantity
-    assert_equal 13635, invoice_item.unit_price
-    assert_equal 'time', invoice_item.created_at
-    assert_equal 'time2', invoice_item.updated_at
   end
 end
--- test/transaction_test.rb
+++ test/transaction_test.rb
@@ -12,9 +12,7 @@ class TransactionTest < Minitest::Test
     assert_equal 1, transaction.id
     assert_equal 1, transaction.invoice_id
     assert_equal 4654405418249632, transaction.credit_card_number
-    assert_equal '', transaction.credit_card_expiration_date
+    assert_equal nil, transaction.credit_card_expiration_date
     assert_equal 'success', transaction.result
-    assert_equal 'time', transaction.created_at
-    assert_equal 'time2', transaction.updated_at
   end
 end
