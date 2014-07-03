88d28fd8173ddd38c2bcb7ad8fe09313c9efd6d0
Andy Mention <amention@gmail.com>
2014-07-03 10:47:29 -0600

    Clean up items and invoices in Merchant

--- lib/merchant.rb
+++ lib/merchant.rb
@@ -7,38 +7,39 @@ class Merchant
   attr_reader :id,
               :name,
               :created_at,
-              :updated_at
-              :items
+              :updated_at,
+              :items,
+              :engine
 
   def initialize(data, merchant_repo_ref)
     @id                = data[:id].to_i
     @name              = data[:name]
     @created_at        = data[:created_at]
     @updated_at        = data[:updated_at]
-    @merchant_repo_ref = merchant_repo_ref
+    @engine            = merchant_repo_ref.engine
   end
 
   def items
-    @merchant_repo_ref.engine.item_repository.find_all_by_merchant_id(id)
+    engine.item_repository.find_all_by_merchant_id(id)
   end
 
   def invoices
-    @merchant_repo_ref.engine.invoice_repository.find_all_by_merchant_id(id)
+    engine.invoice_repository.find_all_by_merchant_id(id)
   end
 
-  def get_invoice_items
-    invoices.collect { |invoice|
- @invoice_item_repository.find_all_by_invoice_id(invoice.id) }
-  end
-
-  def revenue
-    invoice_items = get_invoice_items
-    revenue = 0
-    invoice_items.flatten.each do |invoice_item|
-      revenue += (invoice_item.quantity * invoice_item.unit_price)
-    end
-    revenue
-  end
+  # def get_invoice_items
+ #    invoices.collect { |invoice|
+ # @invoice_item_repository.find_all_by_invoice_id(invoice.id) }
+ #  end
+ #
+ #  def revenue
+ #    invoice_items = get_invoice_items
+ #    revenue = 0
+ #    invoice_items.flatten.each do |invoice_item|
+ #      revenue += (invoice_item.quantity * invoice_item.unit_price)
+ #    end
+ #    revenue
+ #  end
 
   # def revenue(date)
   #   #  returns the total revenue for that merchant for a specific invoice date
--- test/merchant_test.rb
+++ test/merchant_test.rb
@@ -1,6 +1,8 @@
 require './test/test_helper'
 
 class MerchantTest < Minitest::Test
+  attr_reader :merchant
+  
   def setup
     engine = SalesEngine.new('./test/fixtures')
     @merchant = engine.merchant_repository.merchants[0]
@@ -11,18 +13,21 @@ class MerchantTest < Minitest::Test
     assert_equal "Schroeder-Jerde", @merchant.name
   end
 
-  def test_it_finds_items_by_merchant_id
-    assert @merchant.items.length >= 11
+  def test_it_finds_items_for_a_merchant
+    assert merchant.respond_to? :items
+    assert merchant.items
+    assert merchant.items.length >= 11
   end
 
-  def test_it_finds_invoices_by_merchant_id
-    invoices_length = @merchant.invoices.length
-    assert invoices_length >= 1
+  def test_it_finds_invoices_for_a_merchant
+    assert merchant.respond_to? :invoices
+    assert merchant.invoices
+    assert merchant.invoices.length >= 1
   end
 
   def test_it_finds_revenue
     skip
     puts merchant.revenue
-    assert @merchant.revenue >= 1
+    assert merchant.revenue >= 1
   end
 end
