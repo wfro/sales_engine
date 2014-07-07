6adfb95ac750fa4c03dafec7a9664c5c97400b9e
Andy Mention <amention@gmail.com>
2014-07-07 13:22:42 -0600

    Add Date objects to Invoice Item objects

--- test/invoice_item_test.rb
+++ test/invoice_item_test.rb
@@ -12,6 +12,13 @@ class InvoiceItemTest < Minitest::Test
     assert_equal 1, invoice_item.id
   end
 
+  def test_dates_are_converted_to_Date_objects
+    assert invoice_item.created_at
+    assert invoice_item.updated_at
+    assert_equal Date, invoice_item.created_at.class
+    assert_equal Date, invoice_item.updated_at.class
+  end
+
   def test_it_converts_unit_price_to_BigDecimal
     assert_equal BigDecimal('136.35'), invoice_item.unit_price
     assert_equal BigDecimal, invoice_item.unit_price.class
